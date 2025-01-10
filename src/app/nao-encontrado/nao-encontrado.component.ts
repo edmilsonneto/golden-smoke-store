9da8d3f09fd1833b5e3f291ec4863c07f01c87e2
Edmilson Neto <edmilsonneto10@gmail.com>
2025-01-10 07:49:06 -0300

    commit inicial

---
+++ src/app/nao-encontrado/nao-encontrado.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-nao-encontrado',
+    templateUrl: './nao-encontrado.component.html',
+    styleUrls: ['./nao-encontrado.component.css'],
+    standalone: false
+})
+export class NaoEncontradoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
